ea5c537987d6cf9ac903822afd24b14c3f5e1376
alex_di <mirag0505@yandex.ru>
2023-09-17 12:53:12 +0300

    fix examples

--- example/client.js
+++ example/client.js
@@ -1,2 +1,2 @@
-import { pluck } from "test_lybrary_for_browser_and_node";
-console.log(pluck([{ name: "foo" }, { name: "bar" }], "name"));
+import { startScriptBothEnviroment } from "test_lybrary_for_browser_and_node";
+startScriptBothEnviroment();
--- example/server.js
+++ example/server.js
@@ -1,3 +1,6 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { pluck } = require("test_lybrary_for_browser_and_node");
-console.log(pluck([{ name: "foo" }, { name: "bar" }], "name"));
+/* eslint-disable @typescript-eslint/no-var-requires */
+const {
+  startScriptBothEnviroment,
+} = require("test_lybrary_for_browser_and_node");
+
+startScriptBothEnviroment();
