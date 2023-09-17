99df0a5ef74c43475e501fcced711f5e81e999c2
alex_di <mirag0505@yandex.ru>
2023-09-17 12:54:13 +0300

    added script for launch library

--- src/starts/startScriptBothEnviroment.ts
+++ src/starts/startScriptBothEnviroment.ts
@@ -1,35 +1,34 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import { HTMLtoObject } from "../parser/HTMLtoObject";
 
 export const startScriptBothEnviroment = (): void => {
   if (typeof window === "undefined") {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const http = require("http");
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
     const fs = require("fs");
-
-    // Get the file name from command-line arguments
     const fileName = process.argv[2];
 
-    const server = http.createServer((req, res) => {
-      fs.readFile(fileName, "utf8", (err: unknown, data) => {
-        // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-        if (err) {
-          res.writeHead(404);
-          res.end("File not found");
-        } else {
-          res.writeHead(200, { "Content-Type": "text/html" });
-          res.end(data);
-          console.log(HTMLtoObject(data));
-        }
-      });
-    });
-
-    server.listen(3000, () => {
-      console.log(
-        `Server running on http://localhost:3000/ and serving ${fileName}`
-      );
+    fs.readFile(`./${fileName}`, "utf8", (err, data) => {
+      if (err) throw err;
+      HTMLtoObject(data);
     });
   } else {
-    // const htmlString = document.getElementById("htmlInput").value;
+    document.addEventListener("DOMContentLoaded", function () {
+      // Your code here
+      console.log("DOM has loaded!");
+    });
+
+    const textArea: HTMLTextAreaElement = document.getElementById(
+      "htmlInput",
+    ) as HTMLTextAreaElement;
+    const button = document?.getElementById("button");
+
+    button?.addEventListener("click", function () {
+      // Your code here
+      console.log("DOM has loaded!");
+      console.log(HTMLtoObject(textArea?.value));
+    });
+
+    console.log("textArea", textArea);
+    console.log("textArea", button);
   }
 };
